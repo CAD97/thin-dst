ab1c31f06a5f0fdb52c651cdb6de1fa894343760
cad97 <cad97@cad97.com>
2019-11-12 20:10:07 -0500

    ice-checkpoint mk ii

--- tests/tests.rs
+++ tests/tests.rs
@@ -3,54 +3,41 @@
 
 #![allow(unused, clippy::redundant_clone)]
 
-use {
-    std::sync::Arc,
-    thin::{self, make_fat, Thinnable},
-};
+use {std::sync::Arc, thin_dst::*};
 
 #[test]
 fn slice() {
     let slice: Vec<u32> = vec![0, 1, 2, 3, 4, 5];
-    let slice: thin::Box<thin::Slice<u32>> = slice.into();
-    assert_eq!(slice, [0, 1, 2, 3, 4, 5]);
+    let slice: ThinBox<(), u32> = ThinBox::new((), slice);
+    assert_eq!(slice.slice, [0, 1, 2, 3, 4, 5]);
     let slice = slice.clone();
 }
 
-#[derive(Debug, Default)]
-struct Data {
-    node_kind: u16,
-    source_len: u32,
-    children_len: u16,
-}
+type Data = usize;
+#[repr(transparent)]
+#[derive(Debug, Clone)]
+struct Node(ThinArc<Data, Node>);
 
-#[derive(Debug)]
-#[repr(C)]
-struct Node {
-    data: Data, // MUST include length of the following slice!
-    children: [thin::Arc<Node>],
-}
+// NB: the wrapper type is required, as the type alias version
+//     type Node = ThinArc<Data, Node>;
+// is rejected as an infinitely recursive type alias expansion.
 
-unsafe impl Thinnable for Node {
-    type Head = Data;
-    type SliceItem = thin::Arc<Node>;
-    make_fat!();
-    fn get_length(head: &Data) -> usize {
-        head.children_len as usize
+impl Node {
+    fn new(head: Data, children: Vec<Node>) -> Self {
+        Node(ThinArc::new(head, children))
+    }
+
+    fn data(&self) -> usize {
+        self.0.head
     }
 }
 
 #[test]
 fn node() {
-    let a: thin::Arc<Node> = thin::Arc::new(vec![], |_| Data::default());
-    let b: thin::Arc<Node> = thin::Arc::new(vec![], |_| Data::default());
-    let c: thin::Arc<Node> = thin::Arc::new(vec![], |_| Data::default());
-    let boxed: thin::Arc<Node> = thin::Arc::new(
-        vec![a.clone(), b.clone(), c.clone()],
-        |children: &[thin::Arc<Node>]| Data {
-            children_len: 3,
-            node_kind: 1,
-            source_len: children.iter().map(|child| child.data.source_len).sum(),
-        },
-    );
+    let a = Node::new(1, vec![]);
+    let b = Node::new(2, vec![]);
+    let c = Node::new(3, vec![]);
+    let children = vec![a.clone(), b.clone(), c.clone()];
+    let boxed = Node::new(children.iter().map(|node| node.data()).sum(), children);
     dbg!(boxed);
 }
